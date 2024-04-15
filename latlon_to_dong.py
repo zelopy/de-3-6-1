9f26ef346fbf49799e0a6273bc19793679ed1eeb
jheon735 <jheon735@gmail.com>
2024-04-15 18:35:59 +0900

    위경도로 동 매칭하여 목록 파일 생성 	new file:   SeoulBikeWithPollen/seoulbikeinfo.txt 	new file:   SeoulBikeWithPollen/seoulblikeinfo.csv 	modified:   latlon_to_dong.py

--- latlon_to_dong.py
+++ latlon_to_dong.py
@@ -6,8 +6,32 @@ from webdriver_manager.chrome import ChromeDriverManager
 from bs4 import BeautifulSoup
 from selenium.webdriver.common.by import By
 import time
+from shapely.geometry import Point, Polygon, LineString
+import geopandas as gpd
+import pandas as pd
+from pyproj import Transformer
+import pyproj
+import numpy as np
+import folium
+import os
 
-key = "586c7a62426c3933313132574a574944"
+
+names = ["station_id", "station_name", "raccnt", "station_lat", "station_lon"]
+bike = pd.read_csv("seoulbikeinfo.txt", sep='|', names=names, encoding='utf-8', header=0)
+
+gdf_bike = gpd.GeoDataFrame(bike, geometry=gpd.points_from_xy(bike["station_lon"], bike["station_lat"]), crs='epsg:4326')
+
+# point = Point(bike["station_lat"][0], bike["station_lon"])
+
+# TRAN_4326_TO_3857 = Transformer.from_crs("EPSG:4326", "EPSG:3857")
+gdf_dong = gpd.read_file('../DONG_PG.shp', encoding='cp949')
+gdf_bike_dong = gpd.sjoin(gdf_bike.to_crs("epsg:5186"), gdf_dong)
+gdf_bike_dong = gdf_bike_dong.drop(['geometry', 'index_right', 'BASE_DATE', 'ADM_CD'], axis = 1)
+gdf_bike_dong.to_csv('seoulblikeinfo.csv', sep=',')
+
+"""
+# 기본 정보 가져오기
+key = open("key.txt", 'r').read()
 url1 = f'http://openapi.seoul.go.kr:8088/{key}/json/bikeList/1/1000/'
 url2 = f'http://openapi.seoul.go.kr:8088/{key}/json/bikeList/1001/2000/'
 url3 = f'http://openapi.seoul.go.kr:8088/{key}/json/bikeList/2001/2694/'
@@ -22,6 +46,19 @@ ress = res1 + res2 + res3
 print(len(ress))
 # print(json.dumps(response, ensure_ascii=False, indent=3))
 # print(response["rentBikeStatus"]["row"][50]["stationName"])
+
+with open("seoulbikeinfo.txt", 'w', encoding='utf-8') as f:
+    f.write("#station_id | station_name | raccnt | station_lat | station_lon\n9")
+    for res in ress:
+        raccnt = res["rackTotCnt"]
+        try:
+            id, name = res["stationName"].split(". ")
+        except ValueError:
+            id = res["stationName"].split(".")[0]
+            name = ".".join(res["stationName"].split(".")[1:])
+        stationlat = res["stationLatitude"][:-2]
+        stationlon = res["stationLongitude"][:-2]
+        f.write("%s | %s | %s | %s | %s\n" %(id, name, raccnt, stationlat, stationlon))
 
 raccnts = []
 bikecnts = []
@@ -46,19 +83,21 @@ for res in ress:
     stationlons.append(res["stationLongitude"][:-2])
 
 print(raccnts[3])
-# id, name = response1["rentBikeStatus"]["row"][0]["stationName"].split(". ")
-# print(id, name)
+id, name = response1["rentBikeStatus"]["row"][0]["stationName"].split(". ")
+print(id, name)
 
-# lat = response["rentBikeStatus"]["row"][0]["stationLatitude"][:-2]
-# lon = response["rentBikeStatus"]["row"][0]["stationLongitude"][:-2]
-# mapurl = f"http://map.kakao.com/link/map/{lat},{lon}"
+lat = response["rentBikeStatus"]["row"][0]["stationLatitude"][:-2]
+lon = response["rentBikeStatus"]["row"][0]["stationLongitude"][:-2]
+mapurl = f"http://map.kakao.com/link/map/{lat},{lon}"
 
-# res = requests.get(f"https://map.kakao.com/", verify=False, timeout=3)
-# soup = BeautifulSoup(res.text, "html.parser")
-# results = soup.find("a", id='localInfo.map.town')
+res = requests.get(f"https://map.kakao.com/", verify=False, timeout=3)
+soup = BeautifulSoup(res.text, "html.parser")
+results = soup.find("a", id='localInfo.map.town')
 
-# with webdriver.Chrome(service=Service(ChromeDriverManager().install())) as driver:
-#     driver.get(mapurl)
-#     time.sleep(1)
-#     result = driver.find_element(By.ID, "localInfo.map.town")
-#     print(result.text)
+with webdriver.Chrome(service=Service(ChromeDriverManager().install())) as driver:
+    driver.get(mapurl)
+    time.sleep(1)
+    result = driver.find_element(By.ID, "localInfo.map.town")
+    print(result.text)
+
+"""
