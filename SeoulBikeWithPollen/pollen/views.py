1d8a3d7190f616d187d5f00c9ded0b6778c34784
Kim-2301 <seunghhhh2344@gmail.com>
2024-04-17 03:21:34 +0900

    get() 변경 및 index()생성

--- SeoulBikeWithPollen/pollen/views.py
+++ SeoulBikeWithPollen/pollen/views.py
@@ -9,43 +9,32 @@ from django.http import JsonResponse
 class PollenApiView(generics.ListAPIView):
     renderer_classes = [renderers.JSONRenderer]
     serializer_class = PollenAddrCodeSerializer
-    
+
     # 행정구역코드 찾기
-    def get_queryset(self):
-        return PollenAddrCode.objects.filter(
-            #addr1=self.request.query_params.get('addr1', None),
-            addr2=self.request.query_params.get('addr2', None),
-            addr3=self.request.query_params.get('addr3', None)
-        )
+
     
-       
     def get(self, request, *args, **kwargs):
         print('PollenApiView.get() called')
-        queryset = self.get_queryset()
-        if queryset.exists():
-            obj = queryset.first()
-            addr_code = obj.addr_code
+        
+        addr_code = self.request.query_params.get('area_no')
+        pollen_type = self.request.query_params.get('pollen_type', 'pine')  # 꽃가루 타입 기본값은 'pine'입니다.
+        
 
-            result_pine = get_pollen_data('pine', addr_code)
-            # result_weeds = get_pollen_data('weeds', addr_code)
-            # result_oak = get_pollen_data('oak', addr_code)
+        result_pine = get_pollen_data(pollen_type, addr_code)
+        # result_weeds = get_pollen_data('weeds', addr_code)
+        # result_oak = get_pollen_data('oak', addr_code)
 
-            print(result_pine)
-            # print(result_weeds)
-            # print(result_oak)
+        print(result_pine)
+        # print(result_weeds)
+        # print(result_oak)
 
-            # TODO : JSON 병합? 아니면 타입별 구분 호출?
+        # TODO: JSON 병합? 아니면 타입별 구분 호출?
 
-            return Response(result_pine)
-        else:
-            return Response({'error': '데이터가 없습니다.'})
+        return Response(result_pine)
 
-'''
-행정구역코드 목록 조회
-'''
-def result(request, addr2=None):
-    # print(f'result() addr2:{addr2}')
-    if addr2 is None:
-        addr2 = ''
-    addr_list = addr_code_list(addr2)
-    return JsonResponse({'addr_list': addr_list})
+def index(request,addr2=None):
+        if addr2 is None:
+            addr2 = ''
+        addr_data = addr_code_list(addr2)
+        context = {'addr_data': addr_data}
+        return render(request, 'input.html', context)
