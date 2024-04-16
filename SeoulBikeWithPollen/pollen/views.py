03eab7fa0933e980d421cc4f7770fc36e0429a89
Kim-2301 <seunghhhh2344@gmail.com>
2024-04-16 18:10:03 +0900

    더미데이터 생성 및 입력값에 해당하는 addr_code반환 함수 생성

--- SeoulBikeWithPollen/pollen/views.py
+++ SeoulBikeWithPollen/pollen/views.py
@@ -4,6 +4,48 @@ from .models import PollenAddrCode
 from pollen.serializers import *
 from .pollen_api_service import get_pollen_data
 from rest_framework.response import Response
+import json
+from django.http import JsonResponse
+
+class PollenInputView(generics.GenericAPIView):
+    renderer_classes = [renderers.TemplateHTMLRenderer]
+
+    def get(self, request, *args, **kwargs):
+        dummy_data = [
+            {'addr2': '성동구', 'addr3': [
+                {'name': '행당제1동', 'addr_code': '1120056000'},
+                {'name': '행당제2동', 'addr_code': '1120056001'},
+                {'name': '응봉동', 'addr_code': '1120056002'},
+                {'name': '금호1가동', 'addr_code': '1120056003'},
+                {'name': '금호2.3가동', 'addr_code': '1120056004'}
+            ]},
+            {'addr2': '광진구', 'addr3': [
+                {'name': '군자동', 'addr_code': '1121573000'},
+                {'name': '중곡제1동', 'addr_code': '1121574000'},
+                {'name': '중곡제2동', 'addr_code': '1121575000'},
+                {'name': '중곡제3동', 'addr_code': '1121576000'},
+                {'name': '중곡제4동', 'addr_code': '1121577000'}
+            ]},
+            {'addr2': '송파구', 'addr3': [
+                {'name': '거여1동', 'addr_code': '1171053100'},
+                {'name': '거여2동', 'addr_code': '1171053200'},
+                {'name': '마천1동', 'addr_code': '1171054000'},
+                {'name': '마천2동', 'addr_code': '1171055000'},
+                {'name': '방이1동', 'addr_code': '1171056100'}
+            ]}
+        ]
+         
+        return render(request, 'input.html', {'addr_data': dummy_data})
+    
+    def post(self, request, *args, **kwargs):
+        addr2 = request.data.get('addr2')
+        addr3 = request.data.get('addr3')
+        
+        return JsonResponse({
+            'addr2': addr2,
+            'addr3': addr3
+        })
+
 
 class PollenApiView(generics.ListAPIView):
     renderer_classes = [renderers.JSONRenderer]
@@ -12,11 +54,11 @@ class PollenApiView(generics.ListAPIView):
     # 행정구역코드 찾기
     def get_queryset(self):
         return PollenAddrCode.objects.filter(
-            addr1=self.request.query_params.get('addr1', None),
             addr2=self.request.query_params.get('addr2', None),
             addr3=self.request.query_params.get('addr3', None)
-            )
+        )
     
+       
     def get(self, request, *args, **kwargs):
         print('PollenApiView.get() called')
         queryset = self.get_queryset()
@@ -37,4 +79,5 @@ class PollenApiView(generics.ListAPIView):
             return Response(result_pine)
         else:
             return Response({'error': '데이터가 없습니다.'})
+    
 
