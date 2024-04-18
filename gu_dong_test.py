177ee22aaf8047d4a8355b89845647c908ff6eff
dooby99 <102219869+dooby99@users.noreply.github.com>
2024-04-19 07:38:22 +0900

    Update gu_dong_test.py

--- gu_dong_test.py
+++ gu_dong_test.py
@@ -1,15 +1,14 @@
-res1 = requests.get('http://openapi.seoul.go.kr:8088/576f445751686a6a3636776952594f/json/bikeList/1/1000/')
-res2 = requests.get('http://openapi.seoul.go.kr:8088/576f445751686a6a3636776952594f/json/bikeList/1001/2000/')
-res3 = requests.get('http://openapi.seoul.go.kr:8088/576f445751686a6a3636776952594f/json/bikeList/2001/2697/')
+import re
+
+res1 = requests.get('http://openapi.seoul.go.kr:8088/576f445751686a6a3636776952594f/json/bikeList/1/100/')
+
 
 items = []
 items.extend(json.loads(res1.text)['rentBikeStatus']['row'])
-items.extend(json.loads(res2.text)['rentBikeStatus']['row'])
-items.extend(json.loads(res3.text)['rentBikeStatus']['row'])
-        
+
 
 # 정거장 정보 중 몇개는 위치 정보를 찾을 수 없음
-# 현재 총 데이터 중 14개의 구동데이터 빠짐, 동 이름이..ㅜ
+# 현재 총 데이터 중 14개의 구동데이터 빠짐,  동 이름이..ㅜ
 for i in items:
     apiurl = "https://api.vworld.kr/req/address?"
     params = {
@@ -24,22 +23,28 @@ for i in items:
     response = requests.get(apiurl, params=params)
     if response.status_code == 200:
         data = response.json()
+#         print(data)
         if 'response' in data and 'result' in data['response'] and len(data['response']['result']) > 0:
             result = data['response']['result'][0]
             level2 = result['structure']['level2']
-            level4 = result['structure']['level4L']
+            if 'level4A' in result['structure'] and result['structure']['level4A']:
+                level4 = result['structure']['level4A']
+                level4 = re.sub(r'제(\d)', r'\1', level4)
+
+            else:
+                level4 = result['structure']['level4L']
+
+#             print(level4)
             if level4[-1] == "가":
                 level4 = (level4+"동")
         else:
             # 위치 정보 없는 코드 임의로 설정
 #             print("No result found.:")
-            level2 = "마포구"
-            level4 = "망원동"
-            
+            level2 = "종로구"
+            level4 = "사직동"
+
+        # add2 = 구 / add3 = 동
         i['addr2'] = level2
         i['addr3'] = level4 
     else:
         print("Error:", response.status_code)
-
-# for l in items:
-#     print(l)
