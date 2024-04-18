359057c9cea97afed36ea2d1adfe79d178954b7d
jheon735 <jheon735@gmail.com>
2024-04-18 13:15:41 +0900

    requests 예외처리 	modified:   SeoulBikeWithPollen/seoulbike/views.py

--- SeoulBikeWithPollen/seoulbike/views.py
+++ SeoulBikeWithPollen/seoulbike/views.py
@@ -6,8 +6,6 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.urls import reverse
 from django.shortcuts import redirect
 
-
-
 def bike_station_info(request):
     gu_list = SeoulBikeStationInfo.objects.values_list('addr1', flat=True).distinct()
 
@@ -33,25 +31,28 @@ def bike_station_now(request, addr1, addr2):
         ids.append(station.station_id)
     nows = []
     key = open("key.txt", 'r').read()
-    # try:
-    if max(pks) - min(pks) < 900:
-        url = f'http://openapi.seoul.go.kr:8088/{key}/json/bikeList/{min(pks)-10}/{max(pks)+10}/'
-        response = requests.get(url).json()
-        stations = response1["rentBikeStatus"]["row"]
-    else:
-        url1 = f'http://openapi.seoul.go.kr:8088/{key}/json/bikeList/1/1000/'
-        url2 = f'http://openapi.seoul.go.kr:8088/{key}/json/bikeList/1001/2000/'
-        url3 = f'http://openapi.seoul.go.kr:8088/{key}/json/bikeList/2001/2694/'
-        response1 = requests.get(url1).json()
-        response2 = requests.get(url2).json()
-        response3 = requests.get(url3).json()
-        res1 = response1["rentBikeStatus"]["row"]
-        res2 = response2["rentBikeStatus"]["row"]
-        res3 = response3["rentBikeStatus"]["row"]
-        stations = res1 + res2 + res3
+    try:
+        if max(pks) - min(pks) < 900:
+            url = f'http://openapi.seoul.go.kr:8088/{key}/json/bikeList/{min(pks)-10}/{max(pks)+10}/'
+            response = requests.get(url).json()
+            stations = response1["rentBikeStatus"]["row"]
+        else:
+            url1 = f'http://openapi.seoul.go.kr:8088/{key}/json/bikeList/1/1000/'
+            url2 = f'http://openapi.seoul.go.kr:8088/{key}/json/bikeList/1001/2000/'
+            url3 = f'http://openapi.seoul.go.kr:8088/{key}/json/bikeList/2001/2694/'
+            response1 = requests.get(url1).json()
+            response2 = requests.get(url2).json()
+            response3 = requests.get(url3).json()
+            res1 = response1["rentBikeStatus"]["row"]
+            res2 = response2["rentBikeStatus"]["row"]
+            res3 = response3["rentBikeStatus"]["row"]
+            stations = res1 + res2 + res3
 
-    for station in stations:
-        if station['stationId'] in ids:
-            nows.append({"station_id":station["stationId"],"station_name":station["stationName"], "bikecnt":station["parkingBikeTotCnt"], "bikeratio":station["shared"]})
+        for station in stations:
+            if station['stationId'] in ids:
+                nows.append({"station_id":station["stationId"],"station_name":station["stationName"], "bikecnt":station["parkingBikeTotCnt"], "bikeratio":station["shared"]})
+    except requests.exceptions.RequestException as e:
+        error_message = "API 요청 중 오류가 발생했습니다."
+        return render(request, 'seoulbike/error.html', {'error_message': error_message})
 
     return render(request, 'seoulbike/bike_station_now.html', {'nows': nows})
