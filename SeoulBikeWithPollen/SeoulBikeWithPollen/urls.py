c28f967b72fdf8af8bbaaed5fa3598bddf506a14
Kim-2301 <seunghhhh2344@gmail.com>
2024-04-19 01:08:27 +0900

    기본 url 경로 수정

--- SeoulBikeWithPollen/SeoulBikeWithPollen/urls.py
+++ SeoulBikeWithPollen/SeoulBikeWithPollen/urls.py
@@ -20,5 +20,6 @@ from django.urls import include, path
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('map/', include('map.urls')),
-    path('pollen/', include('pollen.urls')),
+    #path('pollen/', include('pollen.urls')),
+    path('',include('pollen.urls'))
 ]
